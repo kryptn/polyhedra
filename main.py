a7377ec7fc9de6039c9b854da1b0a5b96521d32b
Dodeca <dodeca@DodecaBus>
2016-09-07 01:12:14 -0400

    Second Travis-CI Attempt

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from sys import argv
 
-print "Opening the index.html for writing..."
-target = open('index.html', 'w')
-target.truncate()
+print "\nOpening the index.html for writing..."
+with open('out/index.html', 'w') as target:
+    target.truncate()
 
-target.write("<h1>Polyhedra</h1>")
-target.write("\n")
-target.write("<p>a personal Eve tool</p>")
-target.write("\n")
-target.write("<p><small>Hosted on GitHub Pages, Travis-CI was here</p>")
-target.write("\n")
+    target.write("<h1>Polyhedra</h1>")
+    target.write("\n")
+    target.write("<p>a personal Eve tool</p>")
+    target.write("\n")
+    target.write("<p><small>Hosted on GitHub Pages, Travis-CI was here</p>")
+    target.write("\n")
 
-print "Done writing to index.html!\n"
-
-target.close()
+    print "Done writing to index.html!\n"
 
