d0bef2e2a52b2bbf81471a77594b85410113e6be
DuskDragon <duskdragon@gmail.com>
2016-09-11 03:27:22 -0400

    table additions and formatting

--- main.py
+++ main.py
@@ -18,13 +18,26 @@ def css_importer():
     with open('config/styles.css', 'r') as f:
         out_string = '\n'
         for line in f:
-            out_string += "    "+str(line.strip())+"\n"
+            out_string += "    "+str(line.rstrip())+"\n"
     return out_string
+
+def generate_supertable():
+    pass
+
+def generate_sidetable():
+    pass
+
+def generate_killtable():
+    pass
+
 
 current_month_year = "August, 2016"
 killmail_table = """
-<font color="#cccccc">
-<table style="margin-left: auto; margin-right: auto; text-align: left; color: #cccccc" cellspacing="1" >
+<table class="super-table">
+<tbody>
+<tr>
+<td>
+<table class="kb-table">
     <tbody>
         <tr>
             <td>ShipType</td>
@@ -32,23 +45,68 @@ killmail_table = """
             <td>Final Blow</td>
             <td>Location</td>
         </tr>
-        <tr>
+        <tr class="kb-table-row-even-kill">
             <td>Spaceship</td>
-            <td>Character Name</td>
-            <td>Character Name</td>
+            <td>Enemy Character Name</td>
+            <td>Friendly Character Name</td>
+            <td>TVN-FM</td>
+        </tr>
+        <tr class="kb-table-row-odd-loss">
+            <td>Spaceship</td>
+            <td>Friendly Character Name</td>
+            <td>Enemy Character Name</td>
+            <td>TVN-FM</td>
+        </tr>
+        <tr class="kb-table-row-even-kill">
+            <td>Spaceship</td>
+            <td>Enemy Character Name</td>
+            <td>Friendly Character Name</td>
+            <td>TVN-FM</td>
+        </tr>
+        <tr class="kb-table-row-odd-kill">
+            <td>Spaceship</td>
+            <td>Enemy Character Name</td>
+            <td>Friendly Character Name</td>
+            <td>TVN-FM</td>
+        </tr>
+        <tr class="kb-table-row-even-loss">
+            <td>Spaceship</td>
+            <td>Friendly Character Name</td>
+            <td>Enemy Character Name</td>
+            <td>TVN-FM</td>
+        </tr>
+        <tr class="kb-table-row-odd-loss">
+            <td>Spaceship</td>
+            <td>Friendly Character Name</td>
+            <td>Enemy Character Name</td>
             <td>TVN-FM</td>
         </tr>
     </tbody>
 </table>
-</font>
+</td>
+<td>
+<table class="stats-table">
+    <tbody>
+        <tr class="stats-header"><td>Active PVP</td></tr>
+        <tr><td>Characters</td><td>3</td></tr>
+        <tr><td>Ships</td><td>4</td></tr>
+        <tr><td>Total Kills</td><td>30</td></tr>
+    </tbody>
+</table>
 """
+
+    # <script src=https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js></script>
+    # <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    # <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    # <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 output_html = """
 <html>
-    <head/>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Polyhedra Killboard</title>
     <script type="text/javascript">
-        var imageUrls = [""" + url_list_generator() + """];
+        var imageUrls = [""" + url_list_generator() + """        ];
 
       function getImageHtmlCode() {
         var dataIndex = Math.floor(Math.random() * imageUrls.length);
@@ -58,10 +116,10 @@ output_html = """
         return img;
       }
     </script>
-    <style>""" + css_importer() + """</style>
+    <style>""" + css_importer() + """    </style>
     </head>
 
-<body bgcolor="black">
+<body>
 <script type="text/javascript">
   document.write(getImageHtmlCode());
 </script>
