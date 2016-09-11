e9833e142deb403bb577fba3e398eb3e58d982b3
David Bibb <kryptn@gmail.com>
2016-09-11 04:24:07 -0400

    file cleanup
    Removed config folder, updated banner images to https

--- static/js/banner.js
+++ static/js/banner.js
@@ -1,12 +1,13 @@
 banners = [
-'http://i.imgur.com/7xoyXNi.png',
-'http://i.imgur.com/9snwgBB.png',
-'http://i.imgur.com/JEHCad1.png',
-'http://i.imgur.com/ZuywHbC.png',
-'http://i.imgur.com/BkqEvW4.gif',
-'http://i.imgur.com/I6F2msY.png'
+'https://i.imgur.com/7xoyXNi.png',
+'https://i.imgur.com/9snwgBB.png',
+'https://i.imgur.com/JEHCad1.png',
+'https://i.imgur.com/ZuywHbC.png',
+'https://i.imgur.com/BkqEvW4.gif',
+'https://i.imgur.com/I6F2msY.png'
 ];
 
 function getRandomBannerImage(){
     return banners[Math.floor(Math.random() * banners.length)];
 }
+
