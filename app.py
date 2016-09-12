803360480ec217609fd564e1a39eaf50f7c25177
David Bibb <kryptn@gmail.com>
2016-09-12 17:28:57 -0400

    Added example of passing data to template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 
 from flask import Flask, render_template
 from flask_frozen import Freezer
@@ -13,7 +14,9 @@ app.config['FREEZER_RELATIVE_URLS'] = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    shorthand = datetime.now().strftime("%Y-%d-%m")
+    longhand = datetime.now().strftime("%B %d, %Y")
+    return render_template('index.html', shorthand=shorthand, longhand=longhand)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
