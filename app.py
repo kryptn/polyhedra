a5b493a7addecf8eeb36c7eacefa9b372d715fb5
DuskDragon <duskdragon@gmail.com>
2016-09-14 02:50:44 -0400

    comments and argv cleanup     we will need to switch to argparse soon

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ class zKillAPI():
         self.history = {}
         self.most_recent_killID = 0
 
-
         with open('data/characters.json', 'r') as fd:
             self.character_list = json.load(fd)
         if len(self.character_list) > 10:
@@ -36,10 +35,10 @@ class zKillAPI():
                 json.dump([], faild)
             self.history = []
         if len(self.history) == 0:
-            self.most_recent_killID = (14123136-1) #first killmail minus 1 to fetch all
+            self.most_recent_killID = (14123136-1) #first killmail minus 1 to fetch all UPDATE THIS IN FUTURE KILLBOARDS
         else:
             self.most_recent_killID = self.history[0]["killID"]
-        logging.info('zKillAPI.most_recent_killID =' + str(self.most_recent_killID))
+        logging.info('zKillAPI.most_recent_killID=' + str(self.most_recent_killID))
 
     def update_kill_history(self):
         api_call_charID_list = ','.join(str(x) for x in self.character_list.values())
@@ -54,7 +53,8 @@ class zKillAPI():
             raw_api_pages = raw_api_data
             #for a new round of api calls, page 1 item 1 contains the newest killID if it exists
             self.most_recent_killID = raw_api_data[0]["killID"]
-        while len(raw_api_data) != 0:
+            logging.info('zKillAPI.most_recent_killID updated to: ' + str(self.most_recent_killID))
+        while len(raw_api_data) != 0: #ensure there are no further pages
             time.sleep(10) # zkill api can be slow and tends to error out
             current_page += 1
             logging.info('api call: ' +api_call_minus_page_num+str(current_page)+'/')
@@ -75,21 +75,19 @@ def index():
     return render_template('index.html', shorthand=shorthand, longhand=longhand)
 
 if __name__ == "__main__":
+    if len(sys.argv) > 2 and sys.argv[2] == 'debug':
+        logging.basicConfig(level=logging.DEBUG)
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
-        if len(sys.argv) > 2 and sys.argv[2] == 'debug':
-            logging.basicConfig(level=logging.DEBUG)
         freezer.freeze()
 
     elif len(sys.argv) > 1 and sys.argv[1] == 'updatezkill':
-        if len(sys.argv) > 2 and sys.argv[2] == 'debug':
-            logging.basicConfig(level=logging.DEBUG)
-            if len(sys.argv) > 3 and sys.argv[3] == 'forceall':
-                #keep going until first page is empty
-                killmails = zKillAPI()
-                while killmails.update_kill_history() != 1:
-                    time.sleep(10)
-                killmails.write_to_file()
-                exit()
+        if len(sys.argv) > 3 and sys.argv[3] == 'forceall':
+            #keep going until first page is empty
+            killmails = zKillAPI()
+            while killmails.update_kill_history() != 1:
+                time.sleep(10)
+            killmails.write_to_file()
+            exit()
 
         killmails = zKillAPI()
         killmails.update_kill_history()
