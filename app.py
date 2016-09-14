b69464365f1557265f0d308377be4670a0dc4154
DuskDragon <duskdragon@gmail.com>
2016-09-14 02:31:07 -0400

    very rough zKillAPI class

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import sys
+import json
+import logging
+import requests
+import time
 from datetime import datetime
 
 from flask import Flask, render_template
@@ -10,7 +14,59 @@ freezer = Freezer(app)
 app.config['FREEZER_DESTINATION'] = 'out/build'
 app.config['FREEZER_RELATIVE_URLS'] = True
 
+class zKillAPI():
+    def __init__(self):
+        self.character_list = {}
+        self.history = {}
+        self.most_recent_killID = 0
 
+
+        with open('data/characters.json', 'r') as fd:
+            self.character_list = json.load(fd)
+        if len(self.character_list) > 10:
+            raise ValueError("More than 10 values in characters.json, zkill API only allows 10")
+            # maybe later we will go full api insanity and pull two different 10 character calls
+
+        #load current history
+        try:
+            with open('data/history.json', 'r') as fd:
+                self.history = json.load(fd)
+        except IOError:
+            with open('data/history.json', 'a') as faild:
+                json.dump([], faild)
+            self.history = []
+        if len(self.history) == 0:
+            self.most_recent_killID = (14123136-1) #first killmail minus 1 to fetch all
+        else:
+            self.most_recent_killID = self.history[0]["killID"]
+        logging.info('zKillAPI.most_recent_killID =' + str(self.most_recent_killID))
+
+    def update_kill_history(self):
+        api_call_charID_list = ','.join(str(x) for x in self.character_list.values())
+        api_call_frontstr = "http://zkillboard.com/api/character/"
+        api_call_backstr = "/afterKillID/"+str(self.most_recent_killID)+"/no-items/page/"
+        api_call_minus_page_num = api_call_frontstr + api_call_charID_list + api_call_backstr
+        current_page = 1
+        logging.info('api call: ' +api_call_minus_page_num+str(current_page)+'/')
+        raw_api_data = requests.get(api_call_minus_page_num+str(current_page)+'/').json()
+        raw_api_pages = []
+        if len(raw_api_data) != 0:
+            raw_api_pages = raw_api_data
+            #for a new round of api calls, page 1 item 1 contains the newest killID if it exists
+            self.most_recent_killID = raw_api_data[0]["killID"]
+        while len(raw_api_data) != 0:
+            time.sleep(10) # zkill api can be slow and tends to error out
+            current_page += 1
+            logging.info('api call: ' +api_call_minus_page_num+str(current_page)+'/')
+            raw_api_data = requests.get(api_call_minus_page_num+str(current_page)+'/').json()
+            raw_api_pages = raw_api_pages + raw_api_data
+
+        self.history = raw_api_pages + self.history
+        return current_page
+
+    def write_to_file(self):
+        with open('data/history.json', 'w') as outfile:
+            json.dump(self.history, outfile)
 
 @app.route('/')
 def index():
@@ -20,7 +76,29 @@ def index():
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == 'build':
+        if len(sys.argv) > 2 and sys.argv[2] == 'debug':
+            logging.basicConfig(level=logging.DEBUG)
+        #highway to the dangerzone: we are running the forceall code on travisci for this commit only
+        killmails = zKillAPI()
+        while killmails.update_kill_history() != 1:
+            time.sleep(10)
+        killmails.write_to_file()
+        #end of dangerzone
         freezer.freeze()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'updatezkill':
+        if len(sys.argv) > 2 and sys.argv[2] == 'debug':
+            logging.basicConfig(level=logging.DEBUG)
+            if len(sys.argv) > 3 and sys.argv[3] == 'forceall':
+                #keep going until first page is empty
+                killmails = zKillAPI()
+                while killmails.update_kill_history() != 1:
+                    time.sleep(10)
+                killmails.write_to_file()
+                exit()
+
+        killmails = zKillAPI()
+        killmails.update_kill_history()
+        killmails.write_to_file()
     else:
         app.run(debug=True, host='0.0.0.0')
 
