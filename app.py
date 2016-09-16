7a0166a9a4c04604d61c4b43eba6874bcbf18e52
David Bibb <kryptn@gmail.com>
2016-09-16 13:31:39 -0400

    Added per-charater pages

--- app.py
+++ app.py
@@ -163,6 +163,11 @@ class zKillAPI():
             kills[kill['killTime'].split(' ')[0]].append(kill)
         return sorted(kills.items(), key=lambda x: x[0], reverse=True)
 
+    def use_character(self, charid):
+        cs = {v:k for k,v in self.character_list.items()}
+        charname = cs[charid]
+        self.history = [x for x in self.history if charname in x['our_characters'] or charname == x['victim']['characterName']]
+
     def write_to_file(self):
         with open('out/data/history.json', 'w') as outfile:
             json.dump(self.history, outfile)
@@ -180,17 +185,29 @@ class zKillAPI():
         result = {'kills':           self.kill_counts('row-kill'),
                   'losses':          self.kill_counts('row-loss'),
                   'history':         self.kills_by_date(),
+                  'characters':      sorted(self.character_list.items()),
                   'money_lost':      self.kill_sums('row-loss'),
                   'money_killed':    self.kill_sums('row-kill'),
                   'friendlyfire':    self.kill_counts('row-friendlyfire'),
                   'character_count': len(self.character_list)}
         return result
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'charid': None})
+@app.route('/<int:charid>/')
+def index(charid):
     zKill = zKillAPI()
     zKill.build()
+    if charid:
+        zKill.use_character(charid)
     return render_template('index.html', **zKill.data)
+
+@freezer.register_generator
+def index():
+    zKill = zKillAPI()
+    for x in zKill.character_list.values():
+        yield {'charid': x}
+    yield {'charid': None}
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 2 and sys.argv[2] == 'debug':
